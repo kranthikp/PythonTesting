0335fff1b50b4a08bf4b519f7c41d0afe3ee35cd
Kranthi Kumar <kranthi.panda@outlook.com>
2024-04-08 19:01:48 +0530

    Completed creating logger class and implement inheritance -> DRY principle to reuse logs in html report and correct logname called in getLogger

--- pytestsDemo/test_fixtureData.py
+++ pytestsDemo/test_fixtureData.py
@@ -1,9 +1,13 @@
 import pytest
+from pytestsDemo.BaseClass import BaseClass
 
 
 @pytest.mark.usefixtures("dataLoad")
-class TestExample2:
+class TestExample2(BaseClass):
     def test_editProfile(self, dataLoad):
-        print(dataLoad)
-        for index in dataLoad:
-            print(index)
+        log = self.getLogger()
+        # print(dataLoad)
+        log.info((dataLoad[0]))
+        log.info((dataLoad[2]))
+        # for index in dataLoad:
+        #     print(index)
